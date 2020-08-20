9a86f88e2a7b14c15fd715a93345e8689c17024b
Briana Oniru <damioniru16@gmail.com>
2020-08-20 02:41:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,11 @@ def oxford_comma(array)
   array.join
 end
 
-
 def oxford_comma(array)
   array.join(" and ")
 end
 
 def oxford_comma(array)
-  (array[1]..array[2].join("and")
+  
 end
+#  (array[1]..array[2].join("and")#end
